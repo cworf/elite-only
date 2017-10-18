c3ae65733576de0139072cea6b5281d12d5f518c
Colin Worf and Michael Pinaud <michael@>
2017-10-18 16:41:02 -0700

    add referral code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function(){
+  var credit = 0;
   $("#personality").submit(function(event){
     event.preventDefault();
     var firstName = $("#f-name").val();
@@ -6,6 +7,17 @@ $(function(){
     var profession = $("#prof").val();
     var type = parseInt($("input:radio[name=p-type]:checked").val());
     var genre = parseInt($("#genre").val());
-      $("#score-result").append("<center>" + type + genre + "</center>").slideDown();
-  })
-})
+    var result = type + genre;
+    credit = result;
+    $("#score-result").text("$" + credit);
+  });
+
+  $("#referral").submit(function(event){
+    var email = $("#ref-email").val();
+    event.preventDefault();
+    $("#referrals").append(email + "<br>");
+    $(".no-ref").hide();
+    credit = credit + 50;
+    $("#score-result").text(credit);
+  });
+});
