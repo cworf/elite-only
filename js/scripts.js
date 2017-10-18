41124c269ad11366629831eb5f000147d766a6ae
Colin Worf and Michael Pinaud <colin@>
2017-10-18 16:00:06 -0700

    add refer a friend form

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,6 @@ $(function(){
     var profession = $("#prof").val();
     var type = parseInt($("input:radio[name=p-type]:checked").val());
     var genre = parseInt($("#genre").val());
-      $(".card-footer").append(type + genre).show();
+      $("#score-result").append("<center>" + type + genre + "</center>").slideDown();
   })
 })
